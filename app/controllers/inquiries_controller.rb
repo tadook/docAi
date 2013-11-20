860e6e2b6f39a98610901a20732c32785a0a772e
Naoto Usuyama <usuyama.naoto@gmail.com>
2013-11-20 16:01:04 +0900

    added create method

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,4 +1,15 @@
 class InquiriesController < ApplicationController
   def new
   end
+
+  def create
+    @inquiry = Inquiry.new(inquiry_params)
+    @inquiry.save
+    redirect_to @inqiury
+  end
+
+  private
+    def inquiry_params
+      params.require(:inquiry).permit(:age, :sex, :weight, :height)
+    end
 end
